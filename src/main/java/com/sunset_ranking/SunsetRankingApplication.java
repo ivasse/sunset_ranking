c0b32c2869a60cee0516fe38a13a3ce7ac12be0f
Daniel Ivasse <daniel.ivasse@gmail.com>
2017-03-11 04:30:28 +0000

    Setup from start.spring.io

---
+++ src/main/java/com/sunset_ranking/SunsetRankingApplication.java
@@ -0,0 +1,12 @@
+package com.sunset_ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsetRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunsetRankingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sunset_ranking/SunsetRankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sunset_ranking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SunsetRankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
